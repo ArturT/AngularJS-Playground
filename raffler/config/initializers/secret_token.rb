ec9abf4c4603d33e11b04b048efd535c28a894e9
ArturT <arturtrzop@gmail.com>
2013-05-19 15:12:24 +0200

    Rails new raffler. Replace .gitignore with github one

---
+++ raffler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '1cbbb2e94872ca27f05bb29ffb73dac374b0d72b6823e7cd274aef40a840e8127067bd86d398cc7cba376bbba4271a60cf87c32058db8c79a857634a3cc27119'
