b3273ff393cc2f2b3957e0c7b57e779d08369be2
ArturT <arturtrzop@gmail.com>
2013-05-25 21:20:05 +0200

    Refactoring in home index view in egghead app. Add pages_names helper

--- egghead/app/helpers/home_helper.rb
+++ egghead/app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+  def pages_names
+    file_names = Dir.entries(Rails.root+"app/views/pages").select { |f| f.match(/^[0-9]/) }
+    hash_file_names = {}
+    file_names.each do |name|
+      hash_file_names[name.match(/^[0-9]+/)[0].to_i] = name.split('.html.erb').first
+    end
+    hash_file_names.sort.map { |_,v| v }
+  end
 end
