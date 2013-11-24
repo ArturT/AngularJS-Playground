87ba16fe05e492e956329ab0271b9f68e3b7b9e0
ArturT <arturtrzop@gmail.com>
2013-11-24 20:30:52 +0100

    Egghead - 69 gruntjs watch

---
+++ egghead-examples/69_gruntjs_watch/Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+      files: ['journal.txt'],
+      tasks: ['pat']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('pat', function() {
+    grunt.log.writeln('Keep going, you are great!');
+  });
+};
---
+++ egghead-examples/69_gruntjs_watch/README.md
@@ -0,0 +1,18 @@
+# How to create a new package
+
+    # create package.json
+    $ npm init
+
+    # add dependencies to package.json
+    $ npm install grunt --save
+    $ npm install grunt-contrib-watch --save
+
+
+# How to install dependencies
+
+    $ npm install
+
+
+# Grunt watch
+
+    $ grunt watch
